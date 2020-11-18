458900a64382565e3db0d7397f886427001be9e6
haakon <haakon.m.pedersen14@gmail.com>
2020-11-18 10:28:14 +0000

    Ready for me to learn how classes work

--- Blackjack.py
+++ Blackjack.py
@@ -21,10 +21,27 @@ def play_again():
         exit()
 
 
+def deal(player, dealer, deck):
+    for i in range(2):
+        deck = draw(player, deck)
+        deck = draw(dealer, deck)
+
+
 def draw(person, deck):
     person.append(deck[0])
     deck = deck[1:]
     return deck
+
+
+def player_play(player, deck):
+    while counter(player) <= 21:
+        print("You're on: ", counter(player))
+        if input("Hit or stick? (h/s)") == 'h':
+            deck = draw(player, deck)
+            print("\nyou've drawn {}, you're now on {}".format(player[-1], counter(player)))
+            print("Player: ", player)
+        else:
+            break
 
 
 def dealer_play(dealer, deck, player):
@@ -51,36 +68,19 @@ def counter(cards):
                 count += 11
         else:
             count += int(cards[i][0])
-    if count > 21:
-        a = []
+    if count > 21:                      # Really need a better way of doing this
+        a = []                          # this is bad for hard coding a -10
         for i in range(len(cards)):
             a.append(cards[i][0])
         if "Ace" in a:
-            count += -10  # the first term in this list of tuples is an ace replace it with a one
+            count += -10
     return count
 
 
-def game():
-    os.system('CLS')
-    os.system('clear')
-    deck = shuffle()
-    player = []
-    dealer = []
-    for i in range(2):
-        deck = draw(player, deck)
-        deck = draw(dealer, deck)
-    print("\nPlayer: ", player)
-    print("Dealer: ", dealer)
-    while counter(player) <= 21:
-        print("You're on: ", counter(player))
-        if input("Hit or stick? (h/s)") == 'h':
-            deck = draw(player, deck)
-            print("\nyou've drawn {}, you're now on {}".format(player[-1], counter(player)))
-            print("Player: ", player)
-        else:
-            break
-    dealer = dealer_play(dealer, deck, player)
-    if counter(player) > 21:
+def who_wins(player, dealer):
+    if counter(dealer) > 21:
+        print("Dealer went bust! You win")
+    elif counter(player) > 21:
         print("oh no you're bust!, you got ", counter(player))
     elif counter(player) > counter(dealer):
         print("woo you won!")
@@ -90,6 +90,20 @@ def game():
         print("dealer got {} and you got {}".format(counter(dealer), counter(player)))
     else:
         print("oh no dealer got {} and you got {}".format(counter(dealer), counter(player)))
+
+
+def game():
+    #os.system('CLS')               # Comment out for OS
+    os.system('clear')              # top is windows, bottom is linux
+    deck = shuffle()
+    player = []
+    dealer = []
+    deal(player, dealer, deck)
+    print("\nPlayer: ", player)
+    print("Dealer: ", dealer)
+    player_play(player, deck)
+    dealer = dealer_play(dealer, deck, player)
+    who_wins(player, dealer)
     play_again()
 
 
